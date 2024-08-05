a745a9954841d268708afef02224b87c41abd05d
cretelc <cretelc@oregonstate.edu>
2024-08-05 14:41:16 -0700

    first commit, committing the rf global model python script with the chabert function library

---
+++ chabert_global_model_lib.py
@@ -0,0 +1,229 @@
+import numpy as np
+from scipy.constants import electron_mass as me
+from scipy.constants import elementary_charge as q
+from scipy.constants import Avogadro as N_A
+from scipy.constants import Boltzmann as kB
+from scipy.constants import speed_of_light as c
+from scipy.constants import epsilon_0 as eps0
+from scipy.special import jv as bessel 
+
+from scipy.optimize import fsolve
+
+#rint(kB)
+#print(unit('Boltzmann'))
+mu_0 = 1.25663706127e-6
+
+## Need to define inputs 
+# m_dot - mass flow rate
+# V_g   - grid potential, potential difference between plasma and accel 
+# G     - grid geometry vector [R, L]
+# B     - grid transparencies [Bg, Bi]
+
+def calc_hL(L, sig_i, ng):
+    lam_i = 0.03
+    sig_i = 1e-18
+    return 0.86 * (3 + (L/(2 * lam_i))) ** (-1/2) 
+
+def calc_hR(R, sig_i, ng):
+    ''' Return the radial edge-to-center density ratio '''
+    sig_i = 1e-18 
+    lam_i = 0.03
+    return 0.8 * (4 + (R/lam_i))**(-1/2)
+
+# Rate Constants
+def calculate_Kex(Te, Eexc):
+    '''
+    Calculate excitation rates from Chabert 2012
+    Te: electron temperature, K
+    '''
+    Q = kB*Te / q # units=Volt
+    return (1.2921e-13) * np.exp(-Eexc / Q)
+
+def calculate_Kiz(Te, Eiz):
+    '''
+    Ionization rates from Chabert 2012, referencing Fundamentals of Electric Propulsion.
+    
+    Te: electron temperature, K
+    units verified: 
+    terms verified:
+    '''
+    Q = kB*Te / q # units=Volt
+    # Kiz1
+    term11 = (6.73e-15) * (Q**0.5)
+    term12 = 3.97 + 0.643*(Q) - 0.0368 * (Q**2)
+    term13 = np.exp(-Eiz / Q)
+    # Kiz2
+    term21 = (6.73e-15) * (Q**0.5)
+    term22 = (-1.031e-4)*(Q**2) + 6.386 * np.exp(-Eiz/Q)
+
+    Kiz1 = term11*term12*term13
+    Kiz2 = term21*term22
+    return (Kiz1 + Kiz2) / 2
+
+def calculate_Kin(Ti, M, sig_i):
+    # Ti = Tg 
+    #M  = Au2kg(M_au)
+    vi = calculate_v(Ti, M) #np.sqrt(8 * kB*Ti / (np.pi * M))
+    return sig_i * vi
+
+def calculate_Aeff(R, L, sig_i, ng):
+    hR = calc_hR(R, sig_i, ng)
+    hL = calc_hL(L, sig_i, ng)
+    return 2*hR*np.pi * R * L + 2 * hL*np.pi*R**2
+
+def calculate_Aeff1(R, L, ng, sig_i, beta_i):
+    hR = calc_hR(R, sig_i, ng)
+    hL = calc_hL(L, sig_i, ng)
+    return 2*hR*np.pi * R * L + (2-beta_i) * hL*np.pi*R**2
+
+# Plasma values
+def calculate_uB(Te, M):
+    ''' 
+    Calculate the Bohm velocity 
+    verified
+    '''
+    return (kB*Te / M)**0.5
+
+def calculate_k(k0, eps_p):
+    return k0 * eps_p**(1/2)
+
+def calculate_k0(f):
+    w = 2*np.pi * f
+    return w / c
+
+def calculate_eps_p(wpe, f, mu_m):
+    w = 2*np.pi * f
+    return 1 - ((wpe**2) / (w*(complex(w,-mu_m))))
+
+def calculate_wpe(n):
+    ''' Calculates electron frequency '''
+    return np.sqrt((n * q**2) / (me * eps0))
+
+def calculate_mu_m(ng, ve, sig_el):
+    ''' Calculates mean collision frequency '''
+    return ng * sig_el * ve
+
+def calculate_v(T, m):
+    ''' Calculates thermal velocity '''
+    return np.sqrt(8*kB*T / (np.pi*m))
+
+# RF Electrical Calculations
+def calculate_Lcoil(Rc, N, lc):
+    ''' Calculates the coil inductances'''
+    return mu_0 * np.pi * (Rc * N)**2 / lc
+
+def calculate_Rind(L, N, R, f, k, eps_p):
+    w = 2 * np.pi * f
+    kR = k*R
+    J0 = bessel(0, kR)
+    J1 = bessel(1, kR)
+    leading_term = (2 * np.pi * N**2) / (L * w * eps0)
+    complex_term = complex(0, kR*J1/(eps_p*J0))
+    return leading_term * complex_term.real
+
+def calculate_Rohm(ne, mu_m, l_cap, A_cap):
+    Rohm = me * mu_m * l_cap / ((q**2)*ne*A_cap)
+    return Rohm
+
+def calculate_Rstoch(ne, Te, ve, A_cap, Vcoil):
+    Rstoch = (me * ve / ((q**2) * ne * A_cap)) * (q * Vcoil / (kB * Te))**(1/2)
+    return Rstoch
+
+def calculate_Rcap(Rohm, Rstoch):
+    return Rohm + Rstoch
+
+def calculate_Lind(L, N, R, Rc, Lcoil, f, k, eps_p):
+    ''' Calculates the inductance of the plasma circuit'''
+    w  = 2 * np.pi * f
+    kR = k*R
+    J0 = bessel(0, kR)
+    J1 = bessel(1, kR)
+    leading_term = (2 * np.pi * N**2) / (L * w * eps0)
+    complex_term = complex(0, kR*J1/(eps_p*J0))
+    return Lcoil * (1 - (R/Rc)**2) + leading_term*complex_term.imag
+
+def calculate_Ploss(vars, M, K_vec, E_vec, uB, Aeff, V):
+    n, ng, Te, Tg  = vars
+    Kiz, Kexc, Kel = K_vec
+    Eiz, Eexc      = E_vec
+    return Eiz*n*ng*Kiz + Eexc*n*ng*Kexc + 3*(me/M)*kB*(Te-Tg)*n*ng*Kel + (7*kB*Te)*n*uB*(Aeff/V)
+
+def calculate_PabsV2(V, Rind, Icoil, f, Lind, Rcap, C=0):
+    w = 2*np.pi * f
+    
+    return (1/2) * (Rind + ((w**2 * Lind * C)**2) * Rcap)*Icoil**2
+
+def calculate_PabsV3(ne, ng, Te,  sig_el, L, N, R, Rc, lc, Icoil, Vcoil, f, l_cap, A_cap, C=0):
+    w = 2 * np.pi * f
+    
+    wpe  = calculate_wpe(ne)
+    ve   = calculate_v(Te, me)
+    mu_m = calculate_mu_m(ng, ve, sig_el)
+    ep   = calculate_eps_p(wpe, f, mu_m)
+    k0   = calculate_k0(f)
+    k    = calculate_k(k0, ep)
+    Lcoil = calculate_Lcoil(Rc, N, lc)
+    Rind = calculate_Rind(L, N, R, f, k, ep)
+    if C != 0:
+        Rohm = calculate_Rohm(ne, mu_m, l_cap, A_cap)
+        Rstoch = calculate_Rstoch(ne, Te, ve, A_cap, Vcoil)
+        Rcap = calculate_Rcap(Rohm, Rstoch)
+        Lind = calculate_Lind(L, N, R, Rc, Lcoil, f, k, ep)
+        Pabs = (1/2) * (Rind + ((w**2 * Lind * C)**2) * Rcap)*Icoil**2
+    else:
+        Pabs = (1/2) * Rind * (Icoil**2)
+
+    return Pabs 
+
+
+def calculate_Pabs(V, Rind, Icoil):
+    ''' Calculate Absorbed power'''
+    #Rind = calculate_Rind(L, N, R, f, k, eps_p)
+    return (1/(2*V) * Rind * (Icoil**2)) 
+
+def calculate_Pabs_cap():
+    return
+
+def calculate_Vol(R, L):
+    ''' Calculates the internal volume of the discharge chamber'''
+    return np.pi * L * R**2
+
+def calculate_Ag(Bg, R):
+    return np.pi * Bg * R**2
+
+def calculate_Gam_g(ng, vg):
+    return (1/4) * ng * vg
+
+def calculate_Lam0(R, L):
+    ''' Calculate the heat diffusion length. '''
+    # verified
+    return (R/2.405) + (L/np.pi)
+
+def calculate_A(R, L):
+    return 2 * np.pi *R**2 + 2 * np.pi *R*L
+
+
+
+
+
+# Conversions
+def eV2K(eV):
+    ''' Return kelvin (K) given electron-volts (eV) '''
+    return 11606*eV
+
+def K2eV(K):
+    ''' Return electron-volts (eV) given kelvin (K)'''
+    return K/11606
+
+def Au2kg(Au):
+    ''' Returns kg given atomic mass units'''
+    return Au * 1.6605402E-27 
+
+def V2J(V):
+    return V*q
+
+def main():
+    print('Testing...')
+
+if __name__== '__main__':
+    main()
---
+++ main_discharge_global_model.py
@@ -0,0 +1,252 @@
+import numpy as np
+from scipy.constants import electron_mass as me
+from scipy.constants import elementary_charge as q
+from scipy.constants import Avogadro as N_A
+from scipy.constants import Boltzmann as kB
+from scipy.constants import speed_of_light as c
+from scipy.constants import epsilon_0 as eps0
+from scipy.special import jv as bessel 
+from scipy.integrate import solve_ivp
+from scipy.optimize import fsolve
+import chabert_global_model_lib as gm
+
+import matplotlib.pyplot as plt
+
+#rint(kB)
+#print(unit('Boltzmann'))
+mu_0 = 1.25663706127e-6
+
+def sccm2mgs(sccm, Ma):
+    return 7.43583e-4 * Ma * sccm
+def sccm2atoms(sccm):
+    return 4.477962e17 * sccm 
+
+def dne_dt(ne, ng, Kiz, uB, Aeff, V):
+    return 
+def dng_dt(Q0,V, n, uB, Aeff1, Kiz, Gamma_g, Ag):
+    return
+
+def collision_processes(ne, ng, Te, M, rate_constants, energies ):
+    Kiz, Kex, Kel = rate_constants
+    Eiz, Eex, Eel = energies
+    ionization = ne*ng*Kiz*Eiz
+    excitation = ne*ng*Kex*Eex
+    elastic_thermal = ne*ng*(3*me/M)*Kel*kB*Te
+    return [ionization, excitation, elastic_thermal]
+
+def energy_wall_loss(ne, ng, Te, M, R, L, sig_i):
+    uB   = gm.calculate_uB(Te, M)
+    Aeff = gm.calculate_Aeff(R, L, sig_i, ng)
+    V    = gm.calculate_Vol(R, L)
+    wall_power = 7*kB*Te*ne*uB*Aeff/V
+    return wall_power
+
+def test_bed_equations(t, vars):
+    ne, ng, EFg, EFe= vars
+    Te = 2*EFe / (3*ne*kB)
+    Tg = 2*EFg / (3*ng*kB)
+    # Constants
+    Tw      = 400
+    Eiz     = 12.127 # V
+    Eiz_J   = gm.V2J(Eiz)
+    Eexc    = 11.6 # V
+    Eexc_J  = gm.V2J(Eexc)
+    
+    sig_i   = 1e-18
+    sig_el  = sig_i
+    kappa   = 0.0057
+    N       = 5
+    R       = 0.06
+    L       = 0.1
+    f       = 13.56e6
+    M       = gm.Au2kg(131)
+    Q0      = 1.2e19
+    beta_i  = 0.7
+    #beta_i = Bi
+    beta_g  = 0.3
+    #beta_g  = Bg
+    Icoil   = 5
+    # Calculated Parameters
+    Kiz   = gm.calculate_Kiz(Te, Eiz)
+    Kel   = 1e-13
+    Kin   = gm.calculate_Kin(Tg, M, sig_i)
+    Kexc  = gm.calculate_Kex(Te, Eexc) # only shows up in Ploss equation
+    V     = gm.calculate_Vol(R, L)
+    Aeff  = gm.calculate_Aeff(R, L, sig_i, ng)
+    Aeff1 = gm.calculate_Aeff1(R, L, ng, sig_i, beta_i) # not a constant
+    A     = gm.calculate_A(R, L)
+    Ag    = gm.calculate_Ag(beta_g, R)
+    uB    = gm.calculate_uB(Te, M)
+    vg    = gm.calculate_v(Tg, M)
+    Gam_g = gm.calculate_Gam_g(ng, vg)
+    Lam0  = gm.calculate_Lam0(R, L)
+
+    # Breakdown of the ne diff. eq.
+    #dne_dt_in = ne*ng*Kiz
+    #dne_dt_out = ne*uB*Aeff/V
+
+    # Breakdown of the Te diff. eq.
+    k0   = gm.calculate_k0(f)
+    wpe  = gm.calculate_wpe(ne)
+    ve   = gm.calculate_v(Te, me)
+    mu_m = gm.calculate_mu_m(ng, ve, sig_el)
+    ep   = gm.calculate_eps_p(wpe, f, mu_m)
+    k    = gm.calculate_k(k0, ep)
+    Rind = gm.calculate_Rind(L, N, R, f, k, ep)
+    Pabs = gm.calculate_Pabs(V, Rind, Icoil)
+
+    # Breakdown of Ploss
+    PlossA = Eiz_J * ne * ng * Kiz 
+    PlossB = Eexc_J * ne * ng * Kexc 
+    PlossC = 3 * (me/M) * kB * (Te-Tg) * ne * ng * Kel
+    PlossD = (7*kB*Te) * ne * uB * (Aeff/V)
+    Ploss_vec = np.array([PlossA, PlossB, PlossC, PlossD])
+    Ploss_per = Ploss_vec / sum(Ploss_vec)
+    Ploss  = PlossA + PlossB + PlossC + PlossD
+
+    dne_dt = ne*ng*Kiz - ne*uB*Aeff/V
+    dng_dt =  (Q0/V) + ne*uB*Aeff1/V - ne*ng*Kiz - Gam_g*Ag/V
+    # neutral power balance equation 
+    dEFg_dt = 3 * (me/M)*kB*(Te-Tg)*ne*ng*Kel + (1/4) * M*(uB**2) * ne*ng*Kin - kappa * ((Tg-Tw)/Lam0) * (A/V)
+
+    dEFe_dt = (Pabs - Ploss)
+    return [dne_dt, dng_dt, dEFg_dt, dEFe_dt]
+
+def equations(t, vars, Eiz, Eexc, M, N, R, Rc, lc, L, Q0, Bi, Bg, Kel, Tw, kappa, Icoil, f, sig_el, sig_i):
+    ne, ng, EFg, EFe= vars
+    Te = 2*EFe / (3*ne*kB)
+    Tg = 2*EFg / (3*ng*kB)
+    Eiz_J   = gm.V2J(Eiz)
+    Eexc_J  = gm.V2J(Eexc)
+    beta_i = Bi
+    beta_g  = Bg
+    # Calculated Parameters
+    Kiz   = gm.calculate_Kiz(Te, Eiz)
+    Kel   = 1e-13
+    Kin   = gm.calculate_Kin(Tg, M, sig_i)
+    Kexc  = gm.calculate_Kex(Te, Eexc) # only shows up in Ploss equation
+    V     = gm.calculate_Vol(R, L)
+    Aeff  = gm.calculate_Aeff(R, L, sig_i, ng)
+    Aeff1 = gm.calculate_Aeff1(R, L, ng, sig_i, beta_i) # not a constant
+    A     = gm.calculate_A(R, L)
+    Ag    = gm.calculate_Ag(beta_g, R)
+    uB    = gm.calculate_uB(Te, M)
+    vg    = gm.calculate_v(Tg, M)
+    Gam_g = gm.calculate_Gam_g(ng, vg)
+    Lam0  = gm.calculate_Lam0(R, L)
+
+    # Breakdown of the Te diff. eq.
+    k0   = gm.calculate_k0(f)
+    wpe  = gm.calculate_wpe(ne)
+    ve   = gm.calculate_v(Te, me)
+    mu_m = gm.calculate_mu_m(ng, ve, sig_el)
+    ep   = gm.calculate_eps_p(wpe, f, mu_m)
+    k    = gm.calculate_k(k0, ep)
+    Rind = gm.calculate_Rind(L, N, R, f, k, ep)
+    #Pabs = gm.calculate_Pabs(V, Rind, Icoil)
+    Vcoil = 20
+    l_cap = lc
+    A_cap = 2*np.pi*Rc*l_cap
+    C     = 1
+    Pabs_cap = gm.calculate_PabsV3(ne, ng, Te, sig_el, L, N, R, Rc, lc, Icoil, Vcoil, f, l_cap, A_cap, C )
+    Pabs = Pabs_cap / V
+
+    # Breakdown of Ploss
+    PlossA = Eiz_J * ne * ng * Kiz 
+    PlossB = Eexc_J * ne * ng * Kexc 
+    PlossC = 3 * (me/M) * kB * (Te-Tg) * ne * ng * Kel
+    PlossD = (7*kB*Te) * ne * uB * (Aeff/V)
+    Ploss_vec = np.array([PlossA, PlossB, PlossC, PlossD])
+    Ploss_per = Ploss_vec / sum(Ploss_vec)
+    Ploss  = PlossA + PlossB + PlossC + PlossD
+
+    dne_dt = ne*ng*Kiz - ne*uB*Aeff/V
+    dng_dt =  (Q0/V) + ne*uB*Aeff1/V - ne*ng*Kiz - Gam_g*Ag/V
+    # neutral power balance equation 
+    dEFg_dt = 3 * (me/M)*kB*(Te-Tg)*ne*ng*Kel + (1/4) * M*(uB**2) * ne*ng*Kin - kappa * ((Tg-Tw)/Lam0) * (A/V)
+    dEFe_dt = (Pabs - Ploss)
+    return [dne_dt, dng_dt, dEFg_dt, dEFe_dt]
+
+## --- Main function ---
+def main():
+    # Initial guesses
+    Te0 = 15     # initial electron temperature, eV
+    Tg0 = 700   # initial gas temperature, K
+    ne0  = 1e16  # initial gas density, m-3
+    ng0 = 1e18  # initial plasma density, m-3
+
+    EFe0 = (3/2) * (ne0*kB*gm.eV2K(Te0))
+    EFg0 = (3/2) * (ng0*kB*Tg0)
+
+    # Model and Plasma constants 
+    sig_i   = 1e-18 # used as the global variable for elastic and cex cross sections
+    sig_el  = sig_i
+    kappa   = 0.0057 # xenon thermal conductivity
+    Eexc    = 11.6
+    Eiz     = 12.127
+    Kel     = 1e-13     # m3 s-1 
+    Mi      = 131
+    M       = gm.Au2kg(Mi)
+
+    # Thruster Geometry 
+    R  = 0.015  # Discharge chamber radius, m
+    L  = 0.030   # Discharge chamber length, m
+    lc = L/2    # Coil length, m
+    t  = 0.001   # Chamber thickness, m
+    Rc = R + t  # Coil radius, m
+    N  = 5      # number of turns
+    Bi = 0.85    # ion transparency
+    Bg = 0.15    # neutral gas transparency
+
+    # Operating points 
+    mdot    = 1.25           # Particle flow rate, sccm 
+    #Q0     = 1.2e18     # particle injection rate, s-1
+    Q0      = sccm2atoms(mdot)
+    Va      = 1500       # grid potential, V
+    lg      = 0.00036    # grid separation, m
+    f       = 13.56e6    # applied frequency, Hz
+    Icoil   = 5          # current amplitude in the coil, A
+    Tw      = 400        # wall temperature, K
+
+    initial_guesses = [gm.eV2K(Te0), Tg0, ne0, ng0]
+    params = (Eiz, Eexc, M, N, R, Rc, lc, L, Q0, Bi, Bg, Kel, Tw, kappa, Icoil, f, sig_el, sig_i)
+             #Eiz, Eexc, M, N, R, Rc, lc, L, Q0, Bi, Bg, Kel, Tw, kappa, Icoil, f, sig_el, sig_i)
+    fig, axs = plt.subplots(1, 2, figsize=(12,6))
+    meth = 'Radau'
+    #sol = solve_ivp(test_bed_equations, [0,0.0005], [ne0, ng0, EFg0, EFe0], method=meth)#, max_step=1/(10*f))
+    sol = solve_ivp(equations, [0,0.001], [ne0, ng0, EFg0, EFe0], args = params, method=meth)#, max_step=1/(10*f))
+    ne_sol = sol.y[0]
+    ng_sol = sol.y[1]
+    Te_sol = 2*sol.y[3] / (3*ne_sol*kB)
+    Tg_sol = 2*sol.y[2] / (3*ng_sol*kB)
+    # Plot Results
+    
+    i = 0
+    t = sol.t * (1e3)
+    axs[i].semilogy(t, sol.y[0], label=f'ne - {meth}')
+    axs[i].semilogy(t, sol.y[1], label=f'ng - {meth}')
+    axs[i].set_xlabel(r'Time [$ms$]')
+    axs[i].set_ylabel(r'Temperature [$eV$]')
+    axs[i].grid(which='both')
+    axs[i].legend()
+
+    i = 1
+    #axs[i].plot(t, sol.y[0], label=r'$Ionization Fraction$')
+    axs[i].semilogy(t, Tg_sol/11606, label=f'Tg - {meth}')
+    axs[i].semilogy(t, Te_sol/11606, label=f'Te - {meth}')
+    axs[i].set_xlabel(r'Time [$ms$]')
+    axs[i].set_ylabel(r'Temperature [$eV$]')
+    axs[i].grid(which='both')
+    axs[i].legend()
+    plt.show()
+    print('Program complete.')
+    print('Steady State values:')
+    print(f"ne = {sol.y[0][-1]:0.3e}")
+    print(f"ng = {sol.y[1][-1]:0.3e}")
+    print(f"fiz= {100*sol.y[0][-1]/(sol.y[0][-1] + sol.y[1][-1]):0.2f} %")
+    print(f"Tg = {Tg_sol[-1]:0.2f} K")
+    print(f"Te = {gm.K2eV(Te_sol[-1]):0.3f} eV")
+
+if __name__ == "__main__":
+    main()
+
