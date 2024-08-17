266fcafe9296798268cda641cd67fda13f4e8ab6
cretelc <cretelc@oregonstate.edu>
2024-08-16 19:10:13 -0700

    Initial conditions set to guarantee stability for Icoil, ne0=5e17

--- main_discharge_global_model.py
+++ main_discharge_global_model.py
@@ -9,6 +9,7 @@ from scipy.special import jv as bessel
 from scipy.integrate import solve_ivp
 from scipy.optimize import fsolve
 import global_model_lib as gm
+from time import perf_counter
 
 import matplotlib.pyplot as plt
 
@@ -143,12 +144,12 @@ def main():
 
     # Start and End Time
     t_start = 0 # s
-    t_end   = 1e-4 # s
+    t_end   = 5e-5 # s
 
     # Initial guesses
     Te0 = 5     # initial electron temperature, eV
     Tg0 = 100   # initial gas temperature, K
-    ne0 = 1e16  # initial gas density, m-3
+    ne0 = 5e17  # initial gas density, m-3
     ng0 = 1e18 # initial plasma density, m-3
 
     EFe0 = (3/2) * (ne0*kB*gm.eV2K(Te0))
@@ -165,125 +166,152 @@ def main():
     M       = gm.Au2kg(Mi)
 
     # Thruster Geometry 
-    R  = 0.06  # Discharge chamber radius, m
-    L  = 0.1   # Discharge chamber length, m
+    R  = 0.0075  # Discharge chamber radius, m
+    L  = 0.015   # Discharge chamber length, m
     lc = L     # Coil length, m
     t  = 0.001   # Chamber thickness, m
-    Rc = 0.07  # Coil radius, m
+    Rc = 0.008  # Coil radius, m
     N  = 5      # number of turns
     Bi = 0.70    # ion transparency
     Bg = 0.30    # neutral gas transparency
 
     # Operating points 
-    #mdot    = 1.25           # Particle flow rate, sccm 
-    Q0      = 1.2e19     # particle injection rate, s-1
-    #Q0      = sccm2atoms(mdot)
+    mdot    = 1.25           # Particle flow rate, sccm 
+    #Q0      = 1.2e19     # particle injection rate, s-1
+    Q0      = sccm2atoms(mdot)
     Va      = 1500       # grid potential, V
     lg      = 0.00036      # grid separation, m
     f       = 13.56e6    # applied frequency, Hz
     Icoil   = 5         # current amplitude in the coil, A
-    Tw      = 300        # wall temperature, K
+    Tw      = 400        # wall temperature, K
 
 
     params = (Eiz, Eexc, M, N, R, Rc, lc, L, Q0, Bi, Bg, Kel, Tw, kappa, Icoil, f, sig_el, sig_i)
     meth = 'Radau'
-    sol_CC = solve_ivp(equations,    [t_start, t_end], [ne0, ng0, EFg0, EFe0], t_eval=np.linspace(t_start, t_end, 1000), args = params, method=meth)
-    #sol_MT = solve_ivp(equations_MT, [t_start, t_end], [ne0, ng0, EFg0, EFe0], t_eval=np.linspace(t_start, t_end, 1000), args = params, method=meth)
-    '''
-    ne_sol_MT = sol_MT.y[0]
-    ng_sol_MT = sol_MT.y[1]
-    Te_sol_MT = 2*sol_MT.y[3] / (3*ne_sol_MT*kB)
-    Tg_sol_MT = 2*sol_MT.y[2] / (3*ng_sol_MT*kB)
-    
-    ne_sol_CC = sol_CC.y[0]
-    ng_sol_CC = sol_CC.y[1]
-    Te_sol_CC = 2*sol_CC.y[3] / (3*ne_sol_CC*kB)
-    Tg_sol_CC = 2*sol_CC.y[2] / (3*ng_sol_CC*kB)
-
-    fig, axs = plt.subplots(1, 2)
-    axs[0].plot(100*(ng_sol_MT - ng_sol_CC)/ng_sol_MT)
-    axs[1].plot(100*(Tg_sol_MT - Tg_sol_CC)/Tg_sol_MT)
-    plt.show()
-    '''
-    # Plot Results
-    
-    ne_sol = sol_CC.y[0]
-    ng_sol = sol_CC.y[1]
-    Te_sol = 2*sol_CC.y[3] / (3*ne_sol*kB)
-    Tg_sol = 2*sol_CC.y[2] / (3*ng_sol*kB)
-    t = sol_CC.t 
-    fig, axs = plt.subplots(2, 1, figsize=(12,6))
-    ax_left1 = axs[0].twinx()
-    ax_left2 = axs[1].twinx()
-    i = 0
-    
-    colors = ["tab:blue", "tab:orange"]
-    
-    ax_left1.semilogy(t*(1e3), ng_sol,  color=colors[0], label=f'ng - {meth}')
-    axs[i].semilogy(t*(1e3), ne_sol,  color=colors[1], label=f'ne - {meth}')
-    axs[i].set_xlabel(r'Time [$ms$]')
-    axs[i].set_ylabel(r'Density [$m^{-3}$]', color=colors[1])
-    ax_left1.set_ylabel(r'Neutral Gas Density [$m^{-3}$]', color=colors[0])
-    axs[i].grid(which='both')
-
-    i = 1
-    colors = ["tab:blue", "tab:orange"]
-    axs[i].plot(t*(1e3), Te_sol/11606, color=colors[1], label=f'Te - {meth}')
-    ax_left2.plot(t*(1e3), Tg_sol, color=colors[0], label=f'Tg - {meth}')
-
-    axs[i].set_xlabel(r'Time [$ms$]')
-    axs[i].set_ylabel(r'Temperature [$eV$]', color=colors[1])
-    ax_left2.set_ylabel(r'Neutral Temperature [$K$]', color=colors[0])
-    axs[i].grid(which='both')
-    
-    print('Program complete.')
-    print('Steady State values:')
-    print(f"ne = {ne_sol[-1]:0.3e}")
-    print(f"ng = {ng_sol[-1]:0.3e}")
-    print(f"fiz= {100*ne_sol[-1]/(ne_sol[-1] + ng_sol[-1]):0.2f} %")
-    print(f"Tg = {Tg_sol[-1]:0.2f} K")
-    print(f"Te = {gm.K2eV(Te_sol[-1]):0.3f} eV")
-    #plt.show()
+    T = 1/(2*np.pi*f)
+    #max_stepsizes = [T, T/3, T/5, T/20]
+    #max_stepsizes_str = ['1/f', '1/3f', '1/5f', '1/20f']
+    runtimes = [0, 0, 0,0]
+    ls = ['-', '--', '-.', ':']
+    fig1, axs1 = plt.subplots(2, 1, figsize=(12,6))
+    ax1_left1 = axs1[0].twinx()
+    ax1_left2 = axs1[1].twinx()
 
 
-    fig, axs = plt.subplots(3,1)
-    i = 0
-    t = sol_CC.t  #t = sol.t * f
-    axs[i].semilogy(t*(1e3), ng_sol, label=f'ng - {meth}')
-    axs[i].semilogy(t*(1e3), ne_sol, label=f'ne - {meth}')
-    axs[i].set_xlabel(r'Time [$ms$]')
-    axs[i].set_ylabel(r'Density [$m^{-3}$]')
-    axs[i].grid(which='both')
-    axs[i].legend()
+    ms = T/10
+    Is = [2]
+    for j,Icoil in enumerate(Is):
+        params = (Eiz, Eexc, M, N, R, Rc, lc, L, Q0, Bi, Bg, Kel, Tw, kappa, Icoil, f, sig_el, sig_i)
+        t0 = perf_counter()
+        #if j == 3:
+        #    sol_CC = solve_ivp(equations,    [t_start, t_end], [ne0, ng0, EFg0, EFe0], t_eval=np.linspace(t_start, t_end, 1000), args = params, method=meth)
+        
+        sol_CC = solve_ivp(equations,    [t_start, t_end], [ne0, ng0, EFg0, EFe0], t_eval=np.linspace(t_start, t_end, 10000), args = params, method=meth, max_step=ms)
+        runtimes[j] = perf_counter() - t0
+        '''
+        ne_sol_MT = sol_MT.y[0]
+        ng_sol_MT = sol_MT.y[1]
+        Te_sol_MT = 2*sol_MT.y[3] / (3*ne_sol_MT*kB)
+        Tg_sol_MT = 2*sol_MT.y[2] / (3*ng_sol_MT*kB)
+        
+        ne_sol_CC = sol_CC.y[0]
+        ng_sol_CC = sol_CC.y[1]
+        Te_sol_CC = 2*sol_CC.y[3] / (3*ne_sol_CC*kB)
+        Tg_sol_CC = 2*sol_CC.y[2] / (3*ng_sol_CC*kB)
 
-    i = 1
-    ax_left = axs[i].twinx()
-    #axs[i].plot(t, sol.y[0], label=r'$Ionization Fraction$')
-    colors = ["tab:blue", "tab:orange", 'k']
-    axs[i].plot(t*(1e3), Te_sol/11606, color=colors[1], label=f'Te - {meth}')
-    ax_left.plot(t*(1e3), Tg_sol, color=colors[0], label=f'Tg - {meth}')
-    axs[i].set_xlabel(r'Time [$ms$]')
-    axs[i].set_ylabel(r'Temperature [$eV$]', color=colors[1])
-    ax_left.set_ylabel(r'Neutral Temperature [$K$]', color=colors[0])
-    axs[i].grid(which='both')
-    #axs[i].legend()
-    
-    i = 2
-    ax_left = axs[i].twinx()
-    Igm = gm.calculate_Icoil(Icoil, 2*np.pi*f, t)
-    axs[i].plot(t*(1e3), Igm, color=colors[2], label=f'Icoil - {meth}')
-    ax_left.plot(t*(1e3), Te_sol/11606, color=colors[1], label=f'Te - {meth}')
-    axs[i].set_xlabel(r'Time [$s$]')
-    axs[i].set_ylabel(r'Coil current [$A$]', color=colors[2])
-    ax_left.set_ylabel(r'Electron Temperature [$eV$]', color=colors[1])
-    axs[i].grid(which='both')
-    #axs[i].set_xlim(0,1e-6)
+        fig, axs = plt.subplots(1, 2)
+        axs[0].plot(100*(ng_sol_MT - ng_sol_CC)/ng_sol_MT)
+        axs[1].plot(100*(Tg_sol_MT - Tg_sol_CC)/Tg_sol_MT)
+        plt.show()
+        '''
+        # Plot Results
+        
+        ne_sol = sol_CC.y[0]
+        ng_sol = sol_CC.y[1]
+        Te_sol = 2*sol_CC.y[3] / (3*ne_sol*kB)
+        Tg_sol = 2*sol_CC.y[2] / (3*ng_sol*kB)
+        t = sol_CC.t 
+        
+        i = 0
+        
+        colors = ["tab:blue", "tab:orange"]
+        
+        ax1_left1.semilogy(t*(1e3), ng_sol,  linestyle=ls[j], color=colors[0], linewidth=1)
+        axs1[i].semilogy(t*(1e3),   ne_sol,  linestyle=ls[j], color=colors[1], linewidth=1, label=f'Icoil={Icoil}')
+        axs1[i].set_xlabel(r'Time [$ms$]')
+        axs1[i].set_ylabel(r'Density [$m^{-3}$]', color=colors[1])
+        ax1_left1.set_ylabel(r'Neutral Gas Density [$m^{-3}$]', color=colors[0])
+        #axs1[i].grid(which='both')
+
+        i = 1
+        colors = ["tab:blue", "tab:orange"]
+        axs1[i].plot(t*(1e3), Te_sol/11606, linestyle=ls[j], color=colors[1], linewidth=1, label=f'Icoil={Icoil}')
+        ax1_left2.plot(t*(1e3), Tg_sol, linestyle=ls[j], linewidth=1, color=colors[0])
+
+        axs1[i].set_xlabel(r'Time [$ms$]')
+        axs1[i].set_ylabel(r'Temperature [$eV$]', color=colors[1])
+        ax1_left2.set_ylabel(r'Neutral Temperature [$K$]', color=colors[0])
+        
+        
+        #print('Program complete.')
+        #print('Steady State values:')
+        #print(f"ne = {ne_sol[-1]:0.3e}")
+        #print(f"ng = {ng_sol[-1]:0.3e}")
+        #print(f"fiz= {100*ne_sol[-1]/(ne_sol[-1] + ng_sol[-1]):0.2f} %")
+        #print(f"Tg = {Tg_sol[-1]:0.2f} K")
+        #print(f"Te = {gm.K2eV(Te_sol[-1]):0.3f} eV")
+        #plt.show()
+
+
+        ''''
+        i = 0
+        t = sol_CC.t  #t = sol.t * f
+        axs2[i].semilogy(t*(1e3), ng_sol, linestyle=ls[j], label=f'ng - {meth}')
+        axs2[i].semilogy(t*(1e3), ne_sol, linestyle=ls[j], label=f'ne - {meth}')
+        axs2[i].set_xlabel(r'Time [$ms$]')
+        axs2[i].set_ylabel(r'Density [$m^{-3}$]')
+        axs2[i].grid(which='both')
+        axs2[i].legend()
+
+        i = 1
+        
+        #axs[i].plot(t, sol.y[0], label=r'$Ionization Fraction$')
+        colors = ["tab:blue", "tab:orange", 'k']
+        axs2[i].plot(t*(1e3), Te_sol/11606, linestyle=ls[j], color=colors[1], label=f'Te - {meth}')
+        ax2_left.plot(t*(1e3), Tg_sol, linestyle=ls[j], color=colors[0], label=f'Tg - {meth}')
+
+        axs2[i].set_xlabel(r'Time [$ms$]')
+        axs2[i].set_ylabel(r'Temperature [$eV$]', color=colors[1])
+        ax2_left.set_ylabel(r'Neutral Temperature [$K$]', color=colors[0])
+        axs2[i].grid(which='both')
+        #axs[i].legend()
+        
+        i = 2
+        #ax2_left = axs2[i].twinx()
+        Igm = gm.calculate_Icoil(Icoil, 2*np.pi*f, t)
+        axs2[i].plot(t*(1e3), Igm, linestyle=ls[j], color=colors[2], label=f'Icoil - {meth}')
+        ax2_left.plot(t*(1e3), Te_sol/11606, linestyle=ls[j], color=colors[1], label=f'Te - {meth}')
+
+        axs2[i].set_xlabel(r'Time [$s$]')
+        axs2[i].set_ylabel(r'Coil current [$A$]', color=colors[2])
+        ax2_left.set_ylabel(r'Electron Temperature [$eV$]', color=colors[1])
+        axs2[i].grid(which='both')
+        #axs[i].set_xlim(0,1e-6)
+        '''
+    #for i,rt in enumerate(runtimes):
+    #    print(f"ne0={Is[i]}, runtime={rt:0.5f}")
+    print(f" runtime={runtimes[0]:0.5f}")
+    axs1[0].grid(which='both')
+    axs1[0].legend()
+    axs1[1].grid(which='both')
+    axs1[1].legend()
+    #fig, ax = plt.subplots(1,1)
+    #ax.plot()
     plt.show()
     
 
 
 if __name__ == "__main__":
-
     main()
     #test(0, 2e-7, 13.56, 1)
 
